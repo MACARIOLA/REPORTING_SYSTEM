3e488105a568b29548b34033c1aff21e7812f48c
Jessalyn Macariola <116423257+MACARIOLA@users.noreply.github.com>
2024-01-27 10:30:58 +0800

    Create signup_student.php

---
+++ php/signup_student.php
@@ -0,0 +1,51 @@
+<?php
+$dbhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rlcmrs_dtb";
+
+$conn = mysqli_connect($dbhost, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection failed" . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Retrieve and sanitize form data
+    $first_name = mysqli_real_escape_string($conn, $_POST["fname"]);
+    $last_name = mysqli_real_escape_string($conn, $_POST["lname"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $course = mysqli_real_escape_string($conn, $_POST["course"]);
+    $student_number = mysqli_real_escape_string($conn, $_POST["stdNumber"]);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // Hash the password
+
+    // SQL query to insert data into the registration_tbl using prepared statement
+    $sql = "INSERT INTO student_registration_tbl (first_name, last_name, email, course, student_number, password) 
+            VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Prepare the statement
+    $stmt = mysqli_prepare($conn, $sql);
+
+    // Bind parameters
+    mysqli_stmt_bind_param($stmt, "ssssss", $first_name, $last_name, $email, $course, $student_number, $password);
+
+    // Execute the query
+    if (mysqli_stmt_execute($stmt)) {
+        // Redirect to login_student.html using JavaScript
+        echo '<script>window.location.href = "../login_student.html";</script>';
+    }
+    
+    else {
+        // Echo the error message along with the SQL error
+        echo "Error: " . mysqli_stmt_error($stmt);
+    }
+
+    // Close the statement
+    mysqli_stmt_close($stmt);
+
+    // Close the database connection
+    mysqli_close($conn);
+}
+?>
