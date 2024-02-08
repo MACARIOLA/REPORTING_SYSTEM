9067b76c552d2792e497a0f71b2315f6d8f004da
Jessalyn Macariola <116423257+MACARIOLA@users.noreply.github.com>
2024-02-08 22:28:14 +0800

    Update student_report_tbl.sql

--- databases/student_report_tbl.sql
+++ databases/student_report_tbl.sql
@@ -7,5 +7,21 @@ CREATE TABLE `student_report_tbl` (
   `professor` varchar(255) NOT NULL,
   `comment_type` varchar(20) NOT NULL,
   `comment_text` text DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Date End` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `student_report_tbl` (`id`, `building`, `room`, `problem`, `course`, `professor`, `comment_type`, `comment_text`, `timestamp`, `Date End`) VALUES
+(1, 'New Building', '203', 'Electricity', 'DICT 3 - 1', 'Ms. Indaleen', 'none', '', '2024-02-07 23:53:05', NULL),
+(2, 'Old Building', 'Comlab 2', 'Ceiling', 'BSIT 4 - 1', 'Mr. Maravilla', 'none', '', '2024-02-07 23:53:18', NULL),
+(3, 'New Building', '201', 'Chemical', 'BSIT 3 - 1', 'Mr. Maravilla', 'none', '', '2024-02-07 23:53:27', NULL),
+(4, 'Old Building', 'Comlab 1', 'Floor', 'BSIT 3 - 1', 'Mr. Miguel', 'none', '', '2024-02-07 23:53:55', NULL),
+(5, 'New Building', '203', 'Defective', 'BSIT 4 - 1', 'Mr. Miguel', 'none', '', '2024-02-07 23:54:03', NULL),
+(6, 'New Building', 'AVR2', 'Damaged', 'BSIT 3 - 1', 'Mr. Miguel', 'none', '', '2024-02-08 03:16:44', NULL);
+
+ALTER TABLE `student_report_tbl`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `student_report_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
